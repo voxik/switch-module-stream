c2413402cf8f026a86d141dcdbbb57db52481868
Jun Aruga <junaruga@users.noreply.github.com>
2019-09-11 18:20:23 +0200

    Add a case ruby stream to switch from ruby:2.6 to ruby:2.5. (#4)
    Install ruby-hivex and ruby-libguestfs depending on only 2.6,
    but not on 2.5 on Fedora 30, to test removed dependencies
    by "yum --allowerasing distro-sync".

--- script/ruby/install-deps-root.sh
+++ script/ruby/install-deps-root.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -ex
 
-# Install dependencies for the installed gem packages.
+# Install RPM package dependencies for the installed gem packages.
 yum -y install \
   gcc \
   make \
   ruby-devel \
   rubygem-rdoc \
   redhat-rpm-config
+
+# Install RPM packages that can depend on one of current or new stream.
+yum -y install ruby-hivex ruby-libguestfs || true
+
+exit 0
